128d6488e42ddce2a54e58866aaa773c4a2e413b
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-30 22:01:04 +0100

    insert data by my method

--- AppRecipe/AppRecipe/Program.cs
+++ AppRecipe/AppRecipe/Program.cs
@@ -11,8 +11,38 @@ namespace AppRecipe
         static void Main(string[] args)
         {
             //(localdb)\MSSQLLocalDB
-            Console.WriteLine("Hello world entityframework");
-            Console.ReadLine();
+            Recipe r = new Recipe();
+            Console.WriteLine("Donnez l'id");
+            r.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Donnez le nom");
+            r.Name = Console.ReadLine();
+            Boolean Test=insert(r);
+            if(Test)
+            {
+                Console.WriteLine("Data inserted");
+            }
+            else
+            {
+                Console.WriteLine("Error no data inserted");
+            }
+        }
+
+        static Boolean insert(Recipe r)
+        {
+            using (RecipesEntities context = new RecipesEntities())
+            {
+                try
+                {
+                    context.Recipes.Add(r);
+                    context.SaveChanges();
+                    return true;
+                }
+                catch(Exception Ex)
+                {
+                    Console.WriteLine("Error : " + Ex.Message);
+                    return false;
+                }
+            }
         }
     }
 }
