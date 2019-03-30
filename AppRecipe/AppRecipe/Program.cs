112ef926176288ee9de4df0569a2c48ecb46013a
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-30 20:19:37 +0100

    using ado.net finnaly

---
+++ AppRecipe/AppRecipe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRecipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(localdb)\MSSQLLocalDB
+            Console.WriteLine("Hello world entityframework");
+            Console.ReadLine();
+        }
+    }
+}
